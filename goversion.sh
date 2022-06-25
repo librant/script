5d4d05e5d673d2579d2a7b1baa24275dc66f8586
shangfengli <shangfengli@tencent.com>
2022-06-25 15:05:35 +0800

    fix go command

--- goversion.sh
+++ goversion.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-cd /usr/bin/go
+cd /usr/bin
 # 1 : 1.17.10
 # 2 : 1.18.2
 if [ $1 == 1 ]; then
 	rm -f go
-	ln -s /root/tools/go1.17.10/bin /usr/bin/go
+	ln -s /root/tools/go1.17.10/bin/go go
 elif [ $1 == 2 ]; then
 	rm -f go
-	ln -s /root/tools/go1.18.2/bin /usr/bin/go
+	ln -s /root/tools/go1.18.2/bin/go go
 else
 	echo "$1 not support"
 fi
 
+# go version
 go version
