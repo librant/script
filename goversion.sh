d3897de1777441b91d2d9a54fb506483ff35e029
shangfengli <shangfengli@tencent.com>
2022-08-06 14:10:41 +0800

    add version

--- goversion.sh
+++ goversion.sh
@@ -5,10 +5,19 @@ cd /usr/bin
 # 2 : 1.18.2
 if [ $1 == 1 ]; then
 	rm -f go
+	rm -f gofmt
 	ln -s /root/tools/go1.17.10/bin/go go
+	ln -s /root/tools/go1.17.10/bin/gofmt gofmt
 elif [ $1 == 2 ]; then
 	rm -f go
+	rm -f gofmt
 	ln -s /root/tools/go1.18.2/bin/go go
+	ln -s /root/tools/go1.18.2/bin/gofmt gofmt
+elif [ $1 == 3 ]; then
+	rm -f go
+	rm -f gofmt
+	ln -s /root/tools/go1.19/bin/go go
+	ln -s /root/tools/go1.19/bin/gofmt gofmt
 else
 	echo "$1 not support"
 fi
